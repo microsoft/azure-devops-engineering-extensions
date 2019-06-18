7118cf4de9e62d3a87b3d2ded55f76b59ba29871
Leah Schwartz <t-leschw@microsoft.com>
2019-06-18 09:15:37 -0700

    formatting for long running validation comment rows

--- pullRequestInsightsTask/CommentContentFactory.ts
+++ pullRequestInsightsTask/CommentContentFactory.ts
@@ -18,12 +18,21 @@ export class CommentContentFactory {
     }
 
     public createTableSection(current: IPipeline, mostRecent: IPipeline, target: Branch, type: string, longRunningValidations: Map<string, number>, thresholdTimes: Map<string, number>): string {
-        if (current.isFailure()){
+        if (current.isFailure()) {
             if (mostRecent.isFailure()) {
-            return messages.failureCommentRow.format(current.getDisplayName(), current.getLink(), String(target.getPipelineFailStreak()), target.getTruncatedName(), type, target.getTruncatedName(), mostRecent.getDisplayName(), mostRecent.getLink());
+                return messages.failureCommentRow.format(current.getDisplayName(), current.getLink(), String(target.getPipelineFailStreak()), target.getTruncatedName(), type, target.getTruncatedName(), mostRecent.getDisplayName(), mostRecent.getLink());
         }
         return messages.successCommentRow.format(current.getDisplayName(), current.getLink(), target.getTruncatedName(), type);
-    }
-    return ""; // TODO
+        }
+        let section: string;
+        for (let index = 0; index < longRunningValidations.size; index++) {
+            let taskId: string = Object.keys(longRunningValidations)[index];
+            if (index == 0) {
+                section = messages.longRunningValidationCommentFirstSectionRow.format(current.getDisplayName(), taskId, String(longRunningValidations.get(taskId)), String(thresholdTimes.get(taskId)), mostRecent.getDisplayName(), mostRecent.getLink());
+            }
+            else {
+                section += messages.longRunningValidationCommentLowerSectionRow.format(taskId, String(longRunningValidations.get(taskId)), String(thresholdTimes.get(taskId)));
+            }
+        }
     }
 }
