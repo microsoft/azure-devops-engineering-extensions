b544cb7f90cbdd345b98d092018dd9a8270ad30f
Leah Schwartz <t-leschw@microsoft.com>
2019-07-19 12:59:10 -0700

    fixed test import statement

--- pullRequestInsightsTask/__e_to_e_tests__/InvokeTest.ts
+++ pullRequestInsightsTask/__e_to_e_tests__/InvokeTest.ts
@@ -1,27 +1,28 @@
-import { PipelineData } from "../PipelineData";
+import { PipelineData } from "../config/PipelineData";
 import { TaskInsights } from "../TaskInsights";
-import tl = require('azure-pipelines-task-lib/task');
+import tl = require("azure-pipelines-task-lib/task");
 
-
-function runInvokeTaskTest() {
-    let data: PipelineData = new PipelineData();
-    data.setAccessKey("**");
-    data.setCurrentSourceCommitIteration("e18f81f64c08bf4214d596816f5bf47f46ea8c20");
-    data.setHostType("build");
-    data.setProjectName("myepsteam");
-    data.setReleaseId(469);
-    data.setBuildId(887);
-    data.setRepository("myepsteam");
-    data.setTeamUri("https://vscsepsteam.visualstudio.com/");
-    data.setPullRequestId(10);
-    data.setDurationPercentile(1);
-    data.setMimimumValidationDurationSeconds(0);
-    data.setMimimumValidationRegressionSeconds(0);
-    data.setTaskTypesForLongRunningValidations(["powershell"]);
-    data.setIsLongRunningValidationFeatureEnabled(true);
-    tl.debug(JSON.stringify(data));
-    let insights = new TaskInsights(data);
-    insights.invoke();
+function runInvokeTaskTest(): void {
+  let data: PipelineData = new PipelineData();
+  data.setAccessKey("**");
+  data.setCurrentSourceCommitIteration(
+    "1919302351f118af5cfa12286c36fc3aa3322b57"
+  );
+  data.setHostType("build");
+  data.setProjectName("AzureDevOps");
+  data.setReleaseId(11156565);
+  data.setBuildId(9947228);
+  data.setRepository("AzureDevOps");
+  data.setTeamUri("https://dev.azure.com/mseng/");
+  data.setPullRequestId(486147);
+  data.setDurationPercentile(1);
+  data.setMimimumValidationDurationSeconds(0);
+  data.setMimimumValidationRegressionSeconds(0);
+  data.setTaskTypesForLongRunningValidations(["powershell"]);
+  data.setIsLongRunningValidationFeatureEnabled(true);
+  tl.debug(JSON.stringify(data));
+  let insights = new TaskInsights(data);
+  insights.invoke();
 }
 
 runInvokeTaskTest();
