fae875d85ea867003a11efb04764451425c2725e
Krishna V <svajjala@microsoft.com>
2019-09-11 22:05:56 -0700

    Fix Step Errors/Warnings count and bump up count

--- Tasks/emailReportTask/tests/__e_to_e_tests__/InvokeTest.ts
+++ Tasks/emailReportTask/tests/__e_to_e_tests__/InvokeTest.ts
@@ -13,7 +13,6 @@ import { PipelineType } from "../../config/pipeline/PipelineType";
 import { ReportProvider } from "../../providers/ReportProvider";
 import { DataProviderFactory } from "../../providers/DataProviderFactory";
 import { HTMLReportCreator } from "../../htmlreport/HTMLReportCreator";
-import { IReportSender } from "../../IReportSender";
 import { IHTMLReportCreator } from "../../htmlreport/IHTMLReportCreator";
 import { Report } from "../../model/Report";
 import { EmailReportViewModel } from "../../model/viewmodel/EmailReportViewModel";
@@ -44,12 +43,6 @@ export class FileWriter {
   }
 }
 
-export class FileSender implements IReportSender {
-  async sendReportAsync(report: Report, htmlReportMessage: string, mailConfiguration: MailConfiguration): Promise<void> {
-    FileWriter.writeToFile(htmlReportMessage, "emailMessage.html");
-  }
-}
-
 export class ReportCreatorWrapper implements IHTMLReportCreator {
   createHtmlReport(report: Report, reportConfiguration: ReportConfiguration): string {
     // Serialize gathered data into xml 
@@ -65,16 +58,14 @@ export class ReportCreatorWrapper implements IHTMLReportCreator {
 export class MockConfigProvider implements IConfigurationProvider {
 
   getPipelineConfiguration(): PipelineConfiguration {
-    //return new PipelineConfiguration(PipelineType.Build, 12345678, "testproject", "testproject", null, null, true, "https://dev.azure.com/mseng", accessKey);
-    return new PipelineConfiguration(PipelineType.Release, 12345678, "testproject", "testproject", 9012345, 1234, true, "https://testaccount.visualstudio.com/", accessKey);
+    return new PipelineConfiguration(PipelineType.Release, 12345678, "testaccount", "testaccount", 56789012, 1234, true, "https://testaccount.visualstudio.com/", accessKey);
   }
 
   getMailConfiguration(): MailConfiguration {
     return new MailConfiguration("[{environmentStatus}] {passPercentage} tests passed",
-      new RecipientsConfiguration("", false, false, false, true),
+      new RecipientsConfiguration("xyz@test.com", false, false, false, false),
       new RecipientsConfiguration("", false, false, false, false),
-      new SmtpConfiguration(smtpUser, smtpPassword, "smtp.live.com", true),
-      "microsoft.com");
+      new SmtpConfiguration(smtpUser, smtpPassword, "smtp.live.com", true), "test.com");
   }
 
   getReportDataConfiguration(): ReportDataConfiguration {
@@ -83,7 +74,7 @@ export class MockConfigProvider implements IConfigurationProvider {
   }
 
   getSendMailCondition(): SendMailCondition {
-    return SendMailCondition.OnFailure;
+    return SendMailCondition.OnNewFailuresOnly;
   }
 }
 
