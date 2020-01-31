285b4f917645a8a6453af73136c38c05eb8fab42
Sai Krishna V <saikrishnav@outlook.com>
2020-01-31 15:56:10 -0800

    Remove Node-Api Hack and Update to Node-Api 10.0 (#27)
    * Remove Node-Api Hack and Update to Node-Api 10.0 with the Serialization fix
    
    * Add Null Checks for inputs and packaging fixes

--- commands/package.js
+++ commands/package.js
@@ -25,20 +25,6 @@ exec(npmInstallCommand, function (error) {
     console.log(`NPM Install Error: ${error}`);
   } else {
     console.log("NPM Install Done");
-
-    console.log("Patching azure-devops-node-api node_modules..[REMOVE THIS AFTER 9.0.2 release]");
-    const serializationJsFilePath = path.resolve(`${common.TaskOutDir}/node_modules/azure-devops-node-api/serialization.js`);
-    let dataToWrite = "";
-    fs.readFile(serializationJsFilePath, 'utf8', function(err, data) {
-        if (err) throw err;
-        const lines = data.split("\r\n");
-        if(lines.length > 216) {
-          lines[215] = "break;"
-          dataToWrite = lines.join("\r\n");
-          fs.writeFileSync(serializationJsFilePath, dataToWrite);
-          fs.writeFileSync(path.resolve(`${__dirname}/../node_modules/azure-devops-node-api/serialization.js`), dataToWrite);
-        }
-      });
   }
 });
 
@@ -61,18 +47,17 @@ if (fs.existsSync(imagesDir)) {
 // Package extension only for prod
 if (common.ReleaseType.toLowerCase() == "prod") {
   console.log(`Run command in ${common.ExtensionOutDir}: tfx extension create --root . --extension-id ${extensionId} --no-prompt`);
-  // process.chdir(common.ExtensionOutDir);
-  // var command = `tfx extension create --root . --extension-id ${extensionId} --no-prompt`;
-  // console.log(`Running: ${command}`);
-  // exec(command, function (error) {
-  //   if (error) {
-  //     console.log(`Package create error: ${error}`);
-  //   } else {
-  //     console.log("Package created");
-  //   }
-  // });
-} else {
-  console.log(`Navigate to ${common.TaskOutDir} and run the tfx upload command on the desired azure devops account to upload task directly. Command: 'tfx login && tfx build tasks upload --task-path .'`);
+  process.chdir(common.ExtensionOutDir);
+  var command = `tfx extension create --root . --extension-id ${extensionId} --no-prompt`;
+  console.log(`Running: ${command}`);
+  exec(command, function (error) {
+    if (error) {
+      console.log(`Package create error: ${error}`);
+    } else {
+      console.log("Package created. Upload the VSIX to Visual Studio Marketplace if you wish to publish the extension.");
+    }
+  });
 }
+console.log(`If you want to upload to AzureDevOps account directly, then Navigate to ${common.TaskOutDir} and run the tfx upload command on the desired azure devops account to upload task directly. Command: 'tfx login && tfx build tasks upload --task-path .'`);
 
 process.chdir(cwd);
