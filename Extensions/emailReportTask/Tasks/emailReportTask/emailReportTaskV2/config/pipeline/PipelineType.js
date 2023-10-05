43117e1570f6278164ae44f1d2c99495768204cb
Madis Kõosaar <mkoosaar@live.co.uk>
2023-10-05 11:24:10 +0300

    cleanup indermeintermediary files

--- Extensions/emailReportTask/Tasks/emailReportTask/emailReportTaskV2/config/pipeline/PipelineType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PipelineType = void 0;
-var PipelineType;
-(function (PipelineType) {
-    PipelineType[PipelineType["Build"] = 0] = "Build";
-    PipelineType[PipelineType["Release"] = 1] = "Release";
-})(PipelineType = exports.PipelineType || (exports.PipelineType = {}));
-//# sourceMappingURL=PipelineType.js.map
